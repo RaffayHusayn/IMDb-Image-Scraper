95f7cdbe76991c135395b0aaa4b379942967cb4f
Raffay Syed <raffayhusayn@gmail.com>
2022-03-09 01:03:04 -0500

    Add animations to input form for color transition and add logic to not disable text field and button if text val is empty

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -1,7 +1,7 @@
 var resultList = document.querySelector('ul.results');
 var urlInput = document.querySelector('input[name=url]')
 
-apiCallBack = function(xhr, callback) {
+apiCallBack = function (xhr, callback) {
     if (xhr.readyState == XMLHttpRequest.DONE) {
         if (xhr.status != 200) {
             let message = xhr.status + ":" + xhr.statusText + ":"
@@ -17,7 +17,7 @@ apiCallBack = function(xhr, callback) {
     }
 }
 
-updateList = function(response) {
+updateList = function (response) {
     resultList.innerHTML = '';
     for (var i = 0; i < response.length; i++) {
         var img = document.createElement("img");
@@ -30,31 +30,34 @@ updateList = function(response) {
     }
 }
 
-makeApiCall = function(url, method, obj, callback) {
+makeApiCall = function (url, method, obj, callback) {
     let xhr = new XMLHttpRequest();
     xhr.open(method, url);
     xhr.onreadystatechange = apiCallBack.bind(null, xhr, callback);
     xhr.send(obj ? obj instanceof FormData || obj.constructor == String ? obj : JSON.stringify(obj) : null);
 }
 
-document.querySelector('button').addEventListener("click", function(event) {
+document.querySelector('button').addEventListener("click", function (event) {
     event.preventDefault();
-    disableButton("submit");
-    disableInput("search");
+    var input = document.getElementById("search").value;
+    if (input.length > 0) {
+        disableButton("submit");
+        disableInput("search");
+    }
     makeApiCall('/main?url=' + urlInput.value, 'POST', null, updateList);
 });
 
-disableButton = function(id){
+disableButton = function (id) {
     document.getElementById(id).disabled = true;
     // setTimeout(function(){document.getElementById(id).disabled = false;},5000);
 }
-enableButton = function(id){
-    document.getElementById(id).disabled =false;
+enableButton = function (id) {
+    document.getElementById(id).disabled = false;
 }
-disableInput = function(id){
+disableInput = function (id) {
     document.getElementById(id).disabled = true;
     // setTimeout(function(){document.getElementById(id).disabled = false;},5000);
 }
-enableInput = function(id){
-    document.getElementById(id).disabled =false;
+enableInput = function (id) {
+    document.getElementById(id).disabled = false;
 }
